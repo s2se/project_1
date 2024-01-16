483f3dbc8c3b013155aaee795cc04f4041b7f956
Christian Mejillano <christian.mejillano1@students.mq.edu.au>
2024-01-16 11:03:16 +1100

    Initial Commit

---
+++ Paper Bag/Paper_BagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paper_BagApp.swift
+//  Paper Bag
+//
+//  Created by Christian Mejillano on 16/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct Paper_BagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
